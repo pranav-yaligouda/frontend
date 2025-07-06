9f240152a8cd2c27c3a293731d7c63159a893a53
pranav-yaligouda <pranavyaligouda7@gmail.com>
2025-07-06 20:50:27 +0530

    deletedd fake references

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+
 
 import { visualizer } from 'rollup-plugin-visualizer';
 
@@ -13,7 +13,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' && componentTagger(),
+    
     mode === 'production' && visualizer({ open: true, gzipSize: true, brotliSize: true }),
   ].filter(Boolean),
   resolve: {
